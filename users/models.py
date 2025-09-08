189efbeeb7f41c2ec928b6a059cbe4ece2580bbf
shurabota <shurabota2801@gmail.com>
2025-09-08 21:43:57 +0500

    first commit

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True, null=True, verbose_name='О себе')
+    avatar = models.ImageField(upload_to='profile/avatars/%Y/%m/%d/', blank=True, null=True, verbose_name='Аватар')
+    created_at = models.ImageField(auto_now_add=True, verbose_name='Создан')
+    
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
+    def __str__(self):
+        return f'Профиль {self.user.username}'
