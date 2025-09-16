7103575192c0f795378cfb4ded85ddb5501a5337
shurabota <shurabota2801@gmail.com>
2025-09-16 22:50:45 +0500

    first commit

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from .forms import UserRegisterForm
 
 def register(request):
-    if request == 'POST':
+    if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
